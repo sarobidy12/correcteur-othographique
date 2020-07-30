7ee2428668e7077380cb37c197b9453d304b2d04
sarobidy12 <sarobidyalliance@outlook.com>
2020-07-30 12:54:44 +0200

    Updates

---
+++ precache-manifest.12f59ec5c6d6259952dc8bee12f35231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a68e26cd8d14f7831c27eb481adfda7",
+    "url": "/correcteur-othographique/index.html"
+  },
+  {
+    "revision": "22f951eee2b2e7978021",
+    "url": "/correcteur-othographique/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "84027599a84a399d2ff1",
+    "url": "/correcteur-othographique/static/css/main.0751745e.chunk.css"
+  },
+  {
+    "revision": "22f951eee2b2e7978021",
+    "url": "/correcteur-othographique/static/js/2.24a4cca6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/correcteur-othographique/static/js/2.24a4cca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84027599a84a399d2ff1",
+    "url": "/correcteur-othographique/static/js/main.970933cc.chunk.js"
+  },
+  {
+    "revision": "8aa9575e46860f493fc3",
+    "url": "/correcteur-othographique/static/js/runtime-main.602d2599.js"
+  }
+]);
